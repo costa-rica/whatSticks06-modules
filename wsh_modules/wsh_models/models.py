0a0abe9ba6f559cc26844b2af960674d3a2745a6
costa-rica <53882857+costa-rica@users.noreply.github.com>
2022-09-08 15:31:07 +0300

    models update: changed weather to weather history.

--- wsh_modules/wsh_models/models.py
+++ wsh_modules/wsh_models/models.py
@@ -72,9 +72,10 @@ class Oura_token(Base):
     def __repr__(self):
         return f'Oura_token(id: {self.id}, token: {self.token})'
 
-class Weather(Base):
-    __tablename__ = 'weather'
-    id = Column(Integer, primary_key = True)
+
+class Weather_history(Base):
+    __tablename__ = 'weather_history'
+    id = Column(Integer, ForeignKey("users.id"),primary_key = True,autoincrement=True)
     time_stamp_utc = Column(DateTime, nullable=False, default=datetime.utcnow)
     lat = Column(Float)
     lon = Column(Float)
@@ -82,57 +83,17 @@ class Weather(Base):
     region_name = Column(Text)
     country_name = Column(Text)
     tz_id = Column(Text)
-    localtime_epoch = Column(Integer)
-    localtime = Column(Text)
-
-# Air Quality Endpoint
-    co = Column(Float)# Carbon Monoxide (μg/m3)
-    o3 = Column(Float)# Ozone (μg/m3)
-    no2 = Column(Float)# Nitrogen dioxide (μg/m3)
-    so2 = Column(Float)# Sulphur dioxide (μg/m3)
-    pm2_5 = Column(Float)# PM2.5 (μg/m3)
-    pm10 = Column(Float)# PM10 (μg/m3)
-    us_epa_index = Column(Integer)#  	US - EPA standard. 
-    gb_defra_index = Column(Integer)# UK Defra Index
-
-# Realtime API i.e. this is the weather
-    last_updated = Column(Text)
-    last_updated_epoch = Column(Text)
-    temp_c = Column(Float)
-    temp_f = Column(Float)
-    feelslike_c = Column(Float)
-    feelslike_f = Column(Float)
-    condition_text = Column(Text)
-    condition_icon = Column(Text)
-    condition_code = Column(Integer)
-    wind_mph = Column(Float)
-    wind_kph = Column(Float)
-    wind_degree = Column(Integer)
-    wind_dir = Column(String)
-    pressure_mb = Column(Float)
-    pressure_in = Column(Float)
-    precip_mm = Column(Float)
-    precip_in = Column(Float)
-    humidity = Column(Integer)
-    cloud = Column(Integer)
-    is_day = Column(Integer)
-    uv = Column(Float)
-    gust_mph = Column(Float)
-    gust_kph = Column(Float)
-
-# Astronomy API
-    sunrise = Column(Text)
+    date = Column(Text)
+    maxtemp_f = Column(Float)
+    mintemp_f = Column(Float)
+    avgtemp_f = Column(Float)
     sunset = Column(Text)
-    moonrise = Column(Text)
-    moonset = Column(Text)
-    moon_phase = Column(Text)
-    moon_illumination = Column(Integer)
+    sunrise = Column(Text)
 
-    note = Column(Text)
-    
     def __repr__(self):
-        return f"Weather(id: {self.id}, " \
-            f"city_location_name: {self.city_location_name}, temp_c: {self.temp_c})"
+        return f"Weather_history(id: {self.id}, date: {self.date}, " \
+            f"city_location_name: {self.city_location_name}, avgtemp_f: {self.avgtemp_f})"
+
 
 
 class Oura_sleep_descriptions(Base):
